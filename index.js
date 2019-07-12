66ef0e3b0062b48526896a7bd8ffb0b4d2113837
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-12 06:53:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // Code your solution in this file.
 
+
+// function lowerCaseDrivers(drivers){
+//   return drivers.map(function(driver){
+//     return driver.toLowerCase();
+//   });
+// }
+
 function lowerCaseDrivers(drivers){
   return drivers.map(driver =>  driver.toLowerCase() );
 }
+
+function nameToAttributes(drivers)
